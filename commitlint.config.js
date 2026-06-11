dfd6fb6a08040a250e00df00551d06617282cdfe
Matt Strom <strom.matt@gmail.com>
2026-06-11 11:54:54 -0700

    chore: remove empty subproject directory and update commitlint rules

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,8 @@
 export default {
 	extends: ['@commitlint/config-conventional'],
+	rules: {
+		'header-max-length': [0, 'always'],
+		'body-max-line-length': [0, 'always'],
+		'subject-case': [0, 'always'],
+	},
 };
